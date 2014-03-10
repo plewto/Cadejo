36bf9f43e7ee32505256a2b303a002e962b0b04c
Steven Jones <plewto@gmail.com>
2014-03-10 05:54:20 -0500

    Include core.clj

---
+++ src/cadejo/core.clj
@@ -0,0 +1,22 @@
+
+(ns cadejo.core
+  (:use [overtone.live])
+  (:require [cadejo.midi.scene])
+  (:require [cadejo.midi.channel])
+  (:require [cadejo.midi.performance])
+  (:require [cadejo.midi.program])
+  (:require [cadejo.midi.pitch-bend])
+  (:require [cadejo.midi.pressure])
+  (:require [cadejo.midi.cc])
+  (:require [cadejo.midi.mono-mode])
+  (:require [cadejo.midi.poly-mode])
+  (:require [cadejo.demo])
+
+  ;; TESTING
+ ; (:require [cadejo.instruments.alias.pp])
+)
+            
+  
+
+
+
