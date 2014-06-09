9766bcb4e0b8992b61ed1a28a0caf9e65bc56253
Steven Jones <plewto@gmail.com>
2014-06-09 00:21:20 -0500

    Added seesaw to project

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
+                 [seesaw "1.4.4"]
                  [overtone "0.9.1"]]
   :main cadejo.core)
