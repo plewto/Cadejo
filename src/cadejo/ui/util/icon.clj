73de762601160553578a327f7fc84758d72e05d5
Steven Jones <plewto@gmail.com>
2014-08-02 23:45:26 -0500

    Added logo icon load utilitiy

--- src/cadejo/ui/util/icon.clj
+++ src/cadejo/ui/util/icon.clj
@@ -15,3 +15,27 @@
     
 (defn icon [name]
   (seesaw.icon/icon (icon-file name)))
+
+
+
+(def logo-path (path/join "resources" "logos"))
+(def logo-extension icon-extension)
+
+(def logo-size {:small 64
+                :medium 128
+                :large 256})
+
+(defn logo-file [iname size]
+  (let [fqn (path/join logo-path 
+                       (format "%s_%s.%s" 
+                               (.toLowerCase (name iname))
+                               (get logo-size size 64)
+                               logo-extension))]
+    (File. fqn)))
+
+(defn logo 
+  ([iname]
+     (logo iname :small))
+  ([iname size]
+     (let [f (logo-file iname size)]
+       (seesaw.icon/icon f))))
