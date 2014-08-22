915ba9c6d2a94a20beeca18e8a60381be153de01
Steven Jones <plewto@gmail.com>
2014-08-22 13:42:48 -0500

    Simplified undo-stack

--- src/cadejo/ui/util/undo_stack.clj
+++ src/cadejo/ui/util/undo_stack.clj
@@ -1,82 +1,64 @@
 (ns cadejo.ui.util.undo-stack
   "Provides undo/redo stack with associated JButton"
   (:require [seesaw.core :as ss])
-  (:require [cadejo.ui.util.icon])
-  (:require [cadejo.util.user-message])
+  (:require [cadejo.util.user-message :as umsg])
   (:require [cadejo.config]))
 
-(defprotocol UndoStackProtocol
 
-  (max-depth 
+(defprotocol UndoStack
+
+  (max-depth
     [this]
-    "Returns the maximum number of undos")
-  
+    "Return maximum number of undo operations")
+
   (is-empty?
     [this]
-    "Boolean true if stack is empty")
+    "Predicate true if stack empty")
 
-  (push-state
+  (push-state!
     [this obj]
-    "Push object to stack, enable button")
-  
-  (pop-state
+    "Push obj to stack, enable buttons")
+
+  (pop-state!
     [this]
     "Pop and return top stack item.
-     If stack initially empty print warning and return nil
-     If stack becomes empty as result of popping disable button")
+     If stack empty print warning, return nil
+     Disable buttons if stack empty after popping value")
 
   (get-button 
     [this]
-    "Returns instance of JButton.
-     The button is enabled/disabled state changes automatically with 
-     stack contents. It is up to the client to add listeners to the button
-     for extracting stack elements."))
+    "Each call to get-button returns a new instance of JButton
+     The buttons enable/disable state changes with stack contents."))
 
-;(def default-maximum-undo-depth 10)
+(defn undo-stack [label]
+  (let [maximum-depth (cadejo.config/maximum-undo-count)
+        stack* (atom [])
+        buttons* (atom nil)]
+    (reify UndoStack
 
-(defrecord UndoStack [maximum-depth stack* button]
-  UndoStackProtocol
+      (max-depth [this] maximum-depth)
 
-  (max-depth [this]
-    maximum-depth)
+      (is-empty? [this]
+        (zero? (count @stack*)))
 
-  (is-empty? [this]
-    (= 0 (count @stack*)))
-  
-  (push-state [this obj]
-    (swap! stack* (fn [n](conj n obj)))
-    (while (> (count @stack*) maximum-depth)
-      (swap! stack* butlast))
-    (ss/config! button :enabled? true)
-    @stack*)
+      (push-state! [this obj]
+        (swap! stack* (fn [n](conj n obj)))
+        (doseq [b @buttons*]
+          (.setEnabled b true))
+        @stack*)
 
-  (pop-state [this]
-    (if (not (.is-empty? this))
-      (let [rs (first @stack*)]
-        (swap! stack* rest)
-        (ss/config! button :enabled? (not (.is-empty? this)))
-        rs)
-      (do 
-        (cadejo.util.user-message/warning "Nothing to undo")
-        nil)))
+      (pop-state! [this]
+        (if (not (.is-empty? this))
+          (let [rs (first @stack*)]
+            (swap! stack* rest)
+            (doseq [b @buttons*]
+              (.setEnabled b (not (.is-empty? this))))
+            rs)
+          (umsg/warning (format "Nothing to %s" label))))
 
-  (get-button [this]
-    button))
-
-
-(defn undo-stack [label & {:keys [as-icon max-depth]
-                           :or {as-icon false
-                                max-depth (cadejo.config/maximum-undo-count)}}]
-  "Creates instance of UndoStack.
-   label     - button text or icon name
-   as-icon   - Boolean, if as-icon is false button text is set to label
-               argument. If as-icon is true label is used as icon name 
-               relative to resources/icon. Default false.
-   max-depth - int, maximum undos, default 10"
-
-  (let [b (if as-icon 
-            (ss/button :icon (cadejo.ui.util.icon/icon label) :enabled? false)
-            (ss/button :text label :enabled? false))]
-    (UndoStack. max-depth 
-                (atom '())
-                b)))
+      (get-button [this]
+        (let [jb (ss/button :text label)]
+          (swap! buttons* (fn [n](conj n jb)))
+          jb)))))
+                
+     
