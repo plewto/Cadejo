f4487b619950f24ec201624df21591df4d2870f3
Steven Jones <plewto@gmail.com>
2014-08-25 23:53:43 -0500

    Adds toggle buttons with standard icons

--- src/cadejo/ui/util/factory.clj
+++ src/cadejo/ui/util/factory.clj
@@ -1,4 +1,5 @@
 (ns cadejo.ui.util.factory
+  (:use [cadejo.ui.util.icon :only [icon]])
   (:require [seesaw.core :as ss])
   (:require [seesaw.border :as ssb])
   (:import javax.swing.BorderFactory))
@@ -30,9 +31,6 @@
      (ssb/compound-border
       (bevel)
       (padding pad))))
-     
-
-
 
 (defn line 
   ([padding]
@@ -46,17 +44,45 @@
 ;; Buttons
 ;;
 
-(defn init-button [& {:keys [id-suffix]
-                      :or {id-suffix ""}}]
-  (ss/button :text "Init"
-             :id (keyword (format "jb-init-%s" id-suffix))))
+;; Return JToggleButton with 2-icons
+(defn toggle [i-unselected i-selected]
+  (let [b (ss/toggle)]
+    (.setIcon b i-unselected)
+    (.setSelectedIcon b i-selected)
+    b))
 
-(defn delete-button [& {:keys [id-suffix]
-                        :or {id-suffix ""}}]
-  (ss/button :text "X" 
-             :id (keyword (format "jb-delete-%s" id-suffix))))
 
-(defn reset-button [& {:keys [id-suffix]
-                       :or {id-suffix ""}}]
-  (ss/button :text "--"
-             :id (keyword (format "jb-reset-%s" id-suffix))))
+;; Return 'pad' style toggle-button
+;; n - button style 1,2,...
+;;
+(defn pad-button 
+  ([](pad-button 1))
+  ([n]   
+     (let [i1 (icon (format "switches/pad_off_%02d.png" n))
+           i2 (icon (format "switches/pad_on_%02d.png" n))
+           b (toggle i1 i2)]
+       (ss/config! b :size [48 :by 48])
+       b)))
+  
+;; Return pad style filter selection button
+;; ftype - filter type one of :bp :br :lp :hp  
+;; n - button style 1,2,...
+;;
+(defn filter-button 
+  ([ftype](filter-button ftype 1))
+  ([ftype n]
+     (let [i1 (icon (format "switches/filter_%s_off_%02d.png" (name ftype) n))
+           i2 (icon (format "switches/filter_%s_on_%02d.png" (name ftype) n))
+           b (toggle i1 i2)]
+       (ss/config! b :size [48 :by 48]))))
+
+;; Return 'rocker' style toggle button
+;; n - button-style 1,2,3,...
+;; 
+(defn rocker-button
+  ([](rocker-button 1))
+  ([n]
+     (let [i1 (icon (format "switches/rocker_off_%02d.png" n))
+           i2 (icon (format "switches/rocker_on_%02d.png" n))
+           b (toggle i1 i2)]
+       (ss/config! b :size [48 :by 84]))))
