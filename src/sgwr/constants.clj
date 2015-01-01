340557c0a519f9334d10125be7e6ff2d6e2ce102
Steven Jones <plewto@gmail.com>
2014-12-31 23:30:26 -0600

    Initial commit sgwr constants

---
+++ src/sgwr/constants.clj
@@ -0,0 +1,38 @@
+(ns sgwr.constants)
+
+(def +VERSION+ "0.2.0-SNAPSHOT")
+
+(def min-style 0)
+(def max-style 9)
+
+(def style-map {:default 0
+                :dot 0                  ; Point styles
+                :pixel 1
+                :dash 2                 ; used for point and medium line dash
+                :bar 3
+                :diag 4
+                :diag2 5
+                :box 6
+                :cross 7
+                :x 8
+                :triangle 9
+
+                :solid 0                ; line styles
+                :dotted 1
+                :short-dash 3
+                :long-dash 4
+                :center 5               ; dot-dash
+                :dot-dash 5
+                })
+
+(def dot-styles {:default 0
+                 :dot 0
+                 :pixel 1
+                 :dash 2
+                 :bar 3
+                 :diag 4
+                 :diag2 5
+                 :box 6
+                 :cross 7
+                 :x 8
+                 :triangle 9})
